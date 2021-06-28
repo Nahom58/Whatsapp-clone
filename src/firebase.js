c0807a3c13839d7da8aebf47a2d7ae4331f7f725
Nahom Agize <nahom@minerva.kgi.edu>
2021-06-27 21:35:49 -0400

    whatsapp clone build

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDDwcxrrs62OIZlexSSrsl3FoJawopm6iI",
+    authDomain: "whatsapp-clone-f95a4.firebaseapp.com",
+    projectId: "whatsapp-clone-f95a4",
+    storageBucket: "whatsapp-clone-f95a4.appspot.com",
+    messagingSenderId: "905483332076",
+    appId: "1:905483332076:web:7dd6cd3afd3d4da321b8c9"
+  };
